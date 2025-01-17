1f3f69fcd3f2f3525b953b25db33f06994483604
vishalicious213 <vishalicious213@gmail.com>
2025-01-16 22:51:49 -0800

    updated package.json with devStart script to run with env variables

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const checkoutBtn = document.querySelector("#checkoutBtn")
 
+// ⬇️ EVENT LISTENERS ⬇️
+
 checkoutBtn.addEventListener("click", checkout)
+
+// ⬇️ EVENT HANDLERS ⬇️
 
 function checkout() {
     console.log("Checkout")
