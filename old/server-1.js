74811bedca1561fa447b742b95d6f642f779d65d
vishalicious213 <vishalicious213@gmail.com>
2025-01-16 22:07:59 -0800

    added /old/server-1.js to show hx with basic express setup

--- old/server-1.js
+++ old/server-1.js
@@ -22,35 +22,6 @@ const catalog = new Map([
     [6, { priceInCents: 35000, name: "Three hundred fifty"}],
 ])
 
-// create a post request for /create-checkout-session
-app.post("/create-checkout-session", async (req, res) => {
-    try {
-        // create a checkout session with stripe
-        const session = await stripe.checkout.session.create({
-            payment_method_types: ["card"],
-            line_items: req.body.items.map(({ id, quantity }) => {
-                const item = catalog.get(id)
-                return {
-                    price_data: {
-                        currency: "usd",
-                        product_data: {
-                            name: item.name,
-                        },
-                        unit_amount: item.priceInCents
-                    },
-                    quantity: quantity,
-                }
-            }),
-            mode: "payment",
-            success_url: `${process.env.CLIENT_URL}/success.html`,
-            cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
-        })
-        res.json({ url: session.url })
-    } catch (e) {
-        res.status(500).json({ error: e.message })
-    }
-})
-
 // start server
 app.listen(3000, () => {
     console.log("Server started on port 3000")
