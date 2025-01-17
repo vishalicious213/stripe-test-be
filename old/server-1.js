ea438dfff2e5513913ff2169d2cd6e607361e631
vishalicious213 <vishalicious213@gmail.com>
2025-01-16 21:55:45 -0800

    added /old/server-1.js to show hx with basic express setup

---
+++ old/server-1.js
@@ -0,0 +1,28 @@
+// `npm init` to create package.json
+// `npm i stripe express` to install stripe & express
+// create `server.js` and add the code below
+// to run server with environment variable support:
+    // `node --env-file .env ./server.js`
+
+// setup express
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+// setup stripe
+const stripe = require("stripe")(process.env.STRIPE_PVT_KEY)
+
+// inventory data
+const catalog = new Map([
+    [1, { priceInCents: 10000, name: "One hundred"}],
+    [1, { priceInCents: 15000, name: "One hundred fifty"}],
+    [1, { priceInCents: 20000, name: "Two hundred"}],
+    [1, { priceInCents: 25000, name: "Tne hundred fifty"}],
+    [1, { priceInCents: 30000, name: "Three hundred"}],
+    [1, { priceInCents: 35000, name: "Three hundred fifty"}],
+])
+
+// start server
+app.listen(3000, () => {
+    console.log("Server started on port 3000")
+})
