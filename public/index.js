fc56b241b05b5900e91e57bd9eb960832c047c20
vishalicious213 <vishalicious213@gmail.com>
2025-01-21 16:14:48 -0800

    added nav & .catalog to main. added #cartBtn and moved #checkoutBtn to nav

--- public/index.js
+++ public/index.js
@@ -81,7 +81,7 @@ function renderCatalog() {
         `
     })
 
-    document.querySelector("main").innerHTML = catalog
+    document.querySelector(".catalog").innerHTML = catalog
 }
 
 renderCatalog()
